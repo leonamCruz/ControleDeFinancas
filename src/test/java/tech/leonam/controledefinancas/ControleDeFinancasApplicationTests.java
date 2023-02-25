738af44d6d8c8962c5e317a4fa0117027ad8b525
leonam <leonam447@gmail.com>
2023-02-25 11:38:33 -0300

    Initial commit

---
+++ src/test/java/tech/leonam/controledefinancas/ControleDeFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.leonam.controledefinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeFinancasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
